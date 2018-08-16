f2eb7f7b09902bd374adf7b9153ddca69b64f3e1
hotovson <pepe.hotovson@gmail.com>
2018-08-16 10:28:35 +0200

    easy re-record for vcr

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -7,4 +7,9 @@ VCR.configure do |config|
   config.cassette_library_dir = 'spec/support/cassettes'
   config.hook_into :webmock
   config.configure_rspec_metadata!
+
+  # Let's you set default VCR mode with VCR=all for re-recording
+  # episodes. :once is VCR default
+  record_mode = ENV['VCR'] ? ENV['VCR'].to_sym : :once
+  config.default_cassette_options = { record: record_mode }
 end
